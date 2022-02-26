23c587d1502286e1791da3d35935bcbdfb09a170
divyesh67 <diveshhalwai67@gmail.com>
2022-02-26 22:09:52 +0530

    first commit

---
+++ notificationPush.js
@@ -0,0 +1,7 @@
+clevertap.notifications.push({
+    titleText: "Would you like to receive Notification?",
+    bodyText: "Will send you only trusted notifactions and updates",
+    okButtonText: "OK",
+    rejectButtonText: "Cancel",
+    okButtonColor: "#F28046",
+  });
---
+++ scripts.js
@@ -0,0 +1,13 @@
+clevertap.onUserLogin.push({
+    "Site": {
+      "Name": "surya11",
+      "Identity": 1117,
+      "Email": "surya11@gmail.com",
+      "Gender": "M",
+      "DOB": new Date(),
+      "MSG-email": true,
+      "MSG-push": true,
+      "MSG-sms": true,
+      "MSG-whatsapp": true,
+    },
+  });
